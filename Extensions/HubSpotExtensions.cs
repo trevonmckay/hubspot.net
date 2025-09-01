4b34aa9fe72ea18de6cad3e5eab0365d0f2aee5f
Tre`Von McKay <trevon@hireground.io>
2025-09-01 11:32:44 -0400

    Add HubSpotExtensions for conditional property addition
    Introduces static extension methods to simplify adding properties to CreateObjectRequest instances only if values are not null or whitespace. Supports both direct values and values extracted via expressions from source objects.

---
+++ Extensions/HubSpotExtensions.cs
@@ -0,0 +1,59 @@
+﻿using HubSpot.NET.Requests;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+namespace HubSpot.NET
+{
+    public static class HubSpotExtensions
+    {
+        public static TRequest AddPropertyIfNotNullOrWhitespace<TRequest>(this TRequest createObjectRequest, [NotNull] string propertyName, string? value)
+            where TRequest : CreateObjectRequest
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(propertyName);
+
+            createObjectRequest.Properties ??= new Dictionary<string, object>();
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                createObjectRequest.Properties.Add(propertyName, value);
+            }
+
+            return createObjectRequest;
+        }
+
+        public static TRequest AddPropertyIfNotNullOrWhitespace<TRequest, T>(this TRequest createObjectRequest, [NotNull] string propertyName, T source, Expression<Func<T, string?>> sourceMemberExpression)
+            where TRequest : CreateObjectRequest
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(propertyName);
+
+            var getter = sourceMemberExpression.Compile();
+            string? sourcePropValue = getter(source);
+
+            return AddPropertyIfNotNullOrWhitespace<TRequest>(createObjectRequest, propertyName, sourcePropValue);
+        }
+
+        public static TRequest AddPropertyIfNotNull<TRequest>(this TRequest createObjectRequest, [NotNull] string propertyName, object? value)
+            where TRequest : CreateObjectRequest
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(propertyName);
+
+            createObjectRequest.Properties ??= new Dictionary<string, object>();
+            if (value is not null)
+            {
+                createObjectRequest.Properties.Add(propertyName, value);
+            }
+
+            return createObjectRequest;
+        }
+
+        public static TRequest AddPropertyIfNotNull<TRequest, T>(this TRequest createObjectRequest, [NotNull] string propertyName, T source, Expression<Func<T, object?>> sourceMemberExpression)
+            where TRequest : CreateObjectRequest
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(propertyName);
+
+            var getter = sourceMemberExpression.Compile();
+            object? sourcePropValue = getter(source);
+
+            return AddPropertyIfNotNull<TRequest>(createObjectRequest, propertyName, sourcePropValue);
+        }
+    }
+}
