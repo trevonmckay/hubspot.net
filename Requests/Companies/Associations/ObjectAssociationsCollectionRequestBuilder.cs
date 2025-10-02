e7b1f062b04c5d6d8f18ef67f039363a95bd59cb
Tre`Von McKay <trevon@hireground.io>
2025-10-02 17:23:02 -0400

    Overload PutAsync to support AssociationCategory
    Added new overloads for PutAsync to allow specifying AssociationCategory and AssociationTypeId. This provides more flexibility when creating associations and ensures the correct association category is used in requests.

--- Requests/Companies/Associations/ObjectAssociationsCollectionRequestBuilder.cs
+++ Requests/Companies/Associations/ObjectAssociationsCollectionRequestBuilder.cs
@@ -7,15 +7,25 @@ namespace HubSpot.NET.Requests
         internal ObjectAssociationsCollectionRequestBuilder(string baseUrl, BaseClient client)
             : base(baseUrl, client) { }
 
-        public Task PutAsync(string toObjectType, string toObjectId, int assocationTypeId, CancellationToken cancellationToken = default)
+        public Task PutAsync(string toObjectType, string toObjectId, AssociationCategory associationCategory, int assocationTypeId, CancellationToken cancellationToken = default)
         {
             var requestUrl = AppendUrlSegment(toObjectType, toObjectId, assocationTypeId.ToString());
-            return Client.PutJsonAsync<CrmObject>(requestUrl, new { associationCategory = AssociationCategory.HubSpotDefined, assocationTypeId }, cancellationToken);
+            return Client.PutJsonAsync<CrmObject>(requestUrl, new { associationCategory, assocationTypeId }, cancellationToken);
+        }
+
+        public Task PutAsync(string toObjectType, string toObjectId, AssociationCategory associationCategory, AssociationTypeId assocationTypeId, CancellationToken cancellationToken = default)
+        {
+            return PutAsync(toObjectType, toObjectId, associationCategory, (int)assocationTypeId, cancellationToken);
+        }
+
+        public Task PutAsync(string toObjectType, string toObjectId, int assocationTypeId, CancellationToken cancellationToken = default)
+        {
+            return PutAsync(toObjectType, toObjectId, AssociationCategory.HubSpotDefined, assocationTypeId, cancellationToken);
         }
 
         public Task PutAsync(string toObjectType, string toObjectId, AssociationTypeId assocationTypeId, CancellationToken cancellationToken = default)
         {
-            return PutAsync(toObjectType, toObjectId, (int)assocationTypeId, cancellationToken);
+            return PutAsync(toObjectType, toObjectId, AssociationCategory.HubSpotDefined, (int)assocationTypeId, cancellationToken);
         }
 
         public Task DeleteAsync(string toObjectType, string toObjectId, int assocationTypeId, CancellationToken cancellationToken = default)
